8a6baa4b0fe2b9dc2de99cb4d94fe26b05327888
昊 张 <gcrth@outlook.com>
2019-04-13 11:11:12 +0800

    fix one bug all test

--- scanning/test.c
+++ scanning/test.c
@@ -12,6 +12,38 @@ dsfasd auto a++ int b,case
 0
 123
 
+0.1
+0.001
+0.0
+07.7
+08.7
+1.1
+.9e8
+1.2e-7
+1.4E+7
+7.8e9
+
+0X0.FFp-6
+0xafF.1859P-9
+0x.9FFp-2
+
+'sdafffadsg'
+u'585225\\\a'
+L'dfasc\''
+U'asdsc77'
+'\n'
+
+uu
+u8
+L 
+U 
+u 
+
+u"dcgfctyivyhvf\ahvhv"
+u8"dfgadfgadf"
+L"156165sdsa\a"
+"\f"
+
 - -> -- -=
 + += ++
 = ==
